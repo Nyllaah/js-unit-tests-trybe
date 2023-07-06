0bb5eeab6773a208e183221b5e15969853290d61
Nyllaah <nylla.g@hotmail.com>
2023-07-06 19:17:17 -0300

    feat: correction in productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -35,7 +35,7 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste se productDetails é uma função.
     expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
-    expect(Array.isArray(productDetails('Alcool gel', 'Máscara'))).toBe(true);
+    expect(Array.isArray(productDetails('Alcool gel', 'Máscara'))).toBeTruthy();
     // Teste se o array retornado pela função contém dois itens dentro.
     expect(productDetails('Alcool gel', 'Máscara').length).toBe(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
