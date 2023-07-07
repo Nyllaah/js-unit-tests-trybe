3da243655d08dd4e073a9256bfa6fc81f4d0d85f
Nyllaah <nylla.g@hotmail.com>
2023-07-07 16:29:49 -0300

    feat: requirement 10.8 done

--- src/restaurant.js
+++ src/restaurant.js
@@ -4,8 +4,19 @@
 const createMenu = (menu) => {
   let newMenu = {
     fetchMenu: () => menu,
+    consumption: [],
+    order: (item) => {
+      let items = Object.values(newMenu.fetchMenu()).join(' ');
+        if (items.includes(`${item}`)) {
+          newMenu.consumption.push(item);
+        } else {
+          throw new Error('Item indisponível');
+        }
+    },
   };
   return newMenu;
 };
+
+console.log(createMenu({food: {coxinha: 3.90, sanduiche: 9.90}, drinks: {agua: 3.90, cerveja: 6.90}}).order(['coxinha']));
 
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -2,9 +2,14 @@ const createMenu = require('../src/restaurant');
  
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    expect(createMenu({ food: {}, drinks: {} }).fetchMenu).toBeDefined();
+    expect(createMenu().fetchMenu).toBeDefined();
     expect(typeof createMenu().fetchMenu).toBe('function');
     expect(Object.keys(createMenu({ food: {}, drinks: {} }).fetchMenu())).toEqual(['food', 'drinks']);
     expect(createMenu({ food: {}, drinks: {} }).fetchMenu()).toEqual({ food: {}, drinks: {} });
+    expect(createMenu().consumption).toEqual([]);
+    expect(() => { createMenu({food: {coxinha: 3.90, sanduiche: 9.90}, drinks: {agua: 3.90, cerveja: 6.90}}).order('hamburguer') }).toThrow('Item indisponível');
+    // let newMenu = createMenu({food: {coxinha: 3.90, sanduiche: 9.90}, drinks: {agua: 3.90, cerveja: 6.90}}).order(['coxinha', 'agua','cerveja']);
+    // expect(newMenu.consumption).toEqual(['coxinha', 'agua','cerveja']);
+
   });
 });
