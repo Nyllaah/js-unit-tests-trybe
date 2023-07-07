5a0aa5ee010830cbef06fb073a0fe3e3d485d5b1
Nyllaah <nylla.g@hotmail.com>
2023-07-07 19:12:57 -0300

    feat: requirement 10 up to 10.9 working

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,22 +1,40 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
+// const createMenu = (menu) => {
+//   let newMenu = {
+//     fetchMenu: () => menu,
+//     consumption: [],
+//     order: (item) => {
+//       let items = Object.values(newMenu.fetchMenu()).join(' ');
+//         if (items.includes(`${item}`)) {
+//           newMenu.consumption.push(item);
+//         } else {
+//           console.log('Item indisponível');
+//         }
+//     },
+//   };
+//   return newMenu;
+// };
+
 const createMenu = (menu) => {
-  let newMenu = {
+  let inConsumption = [];
+  return {
     fetchMenu: () => menu,
-    consumption: [],
     order: (item) => {
-      let items = Object.values(newMenu.fetchMenu()).join(' ');
-        if (items.includes(`${item}`)) {
-          newMenu.consumption.push(item);
-        } else {
-          throw new Error('Item indisponível');
-        }
+      let items = [...Object.keys(menu.food), ...Object.keys(menu.drinks)];
+      if (items.includes(item)) {
+        inConsumption.push(item);
+        console.log(inConsumption);
+      } else {
+        return 'Item indisponível';
+      }
     },
+    consumption: inConsumption,
   };
-  return newMenu;
 };
 
-console.log(createMenu({food: {coxinha: 3.90, sanduiche: 9.90}, drinks: {agua: 3.90, cerveja: 6.90}}).order(['coxinha']));
+createMenu({ food: { coxinha: 3.90, sanduiche: 9.90 }, drinks: { agua: 3.90, cerveja: 6.90 } }).order('file');
+console.log(createMenu({ food: { coxinha: 3.90, sanduiche: 9.90 }, drinks: { agua: 3.90, cerveja: 6.90 } }).order('file'));
 
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -1,15 +1,30 @@
 const createMenu = require('../src/restaurant');
  
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
-  it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
+  let newMenu = createMenu({food: {coxinha: 3.90, sanduiche: 9.90}, drinks: {agua: 3.90, cerveja: 6.90}});
+  it('Verifica se a função createMenu() retorna um objeto que possui a chave fetchMenu', () => {
     expect(createMenu().fetchMenu).toBeDefined();
+  })
+  it('Verifica se o valor da chave fetchMenu do objeto retornado pela função createMenu() é uma função', () => {
     expect(typeof createMenu().fetchMenu).toBe('function');
+  })
+  it('Verifica se o objeto retornado pela função createMenu({ food: {}, drinks: {} }).fetchMenu() retorna um objeto cujas chaves são somente food e drinks.', () => {
     expect(Object.keys(createMenu({ food: {}, drinks: {} }).fetchMenu())).toEqual(['food', 'drinks']);
+  })
+  it('verifica se o menu passado pra função createMenu() é idêntico ao menu recuperado pela função createMenu({ food: {}, drinks: {} }).fetchMenu().', () => {
     expect(createMenu({ food: {}, drinks: {} }).fetchMenu()).toEqual({ food: {}, drinks: {} });
+  })
+  it('Verifica se a propriedade consumption do objeto retornado pela função createMenu({ food: {}, drinks: {} }), após a criação do menu, retorna um array vazio.', () => {
     expect(createMenu().consumption).toEqual([]);
-    expect(() => { createMenu({food: {coxinha: 3.90, sanduiche: 9.90}, drinks: {agua: 3.90, cerveja: 6.90}}).order('hamburguer') }).toThrow('Item indisponível');
-    // let newMenu = createMenu({food: {coxinha: 3.90, sanduiche: 9.90}, drinks: {agua: 3.90, cerveja: 6.90}}).order(['coxinha', 'agua','cerveja']);
-    // expect(newMenu.consumption).toEqual(['coxinha', 'agua','cerveja']);
-
-  });
+  })
+  it('Verifica se, caso o valor passado por parâmetro para order não conste no objeto passado como parâmetro para createMenu (nem em food ou drinks), o retorno da chave order deve exibir a mensagem "Item indisponível" e não alterar a chave consumption.', () => {
+    expect(createMenu({food: {coxinha: 3.90, sanduiche: 9.90}, drinks: {agua: 3.90, cerveja: 6.90}}).order('hamburguer')).toBe('Item indisponível');
+    newMenu.order('coxinha');
+    expect(newMenu.consumption).toEqual(['coxinha']);
+  })
+  it('Verifica se, ao adicionar três pedidos em sequência, dentre bebidas e comidas, o array consumption contém os itens pedidos.', () => {
+    newMenu.order('agua');
+    newMenu.order('cerveja');
+    expect(newMenu.consumption).toEqual(['coxinha', 'agua', 'cerveja']);
+  })
 });
